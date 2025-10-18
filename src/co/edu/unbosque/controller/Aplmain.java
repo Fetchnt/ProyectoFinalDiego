a05332f2b107c028dee8cd17c80d0ebe45aebd8d
Usuario <Usuario@DESKTOP-ASUSJCCR2>
2025-10-17 23:12:37 -0500

    :v

--- src/co/edu/unbosque/controller/Aplmain.java
+++ src/co/edu/unbosque/controller/Aplmain.java
@@ -3,7 +3,8 @@ package co.edu.unbosque.controller;
 public class Aplmain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println(":v");
 
 	}
 
