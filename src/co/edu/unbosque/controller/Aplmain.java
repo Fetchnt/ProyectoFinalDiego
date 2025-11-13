40ddb2cb322fb91c92d63338ee1e85129a4f9c0b
Usuario <Usuario@DESKTOP-ASUSJCCR2>
2025-11-12 22:23:03 -0500

    javadoc aplmain

--- src/co/edu/unbosque/controller/Aplmain.java
+++ src/co/edu/unbosque/controller/Aplmain.java
@@ -1,12 +1,31 @@
 package co.edu.unbosque.controller;
 
+/**
+ * Clase principal de la aplicación que inicia la ejecución del programa.
+ * <p>
+ * Esta clase contiene el método <code>main</code>, que crea una instancia
+ * del controlador {@link Controller} y llama a su método <code>run</code>
+ * para iniciar la lógica principal de la aplicación.
+ * </p>
+ * 
+ * @author Juan Cabarcas
+ * @version 1.0
+ */
 public class Aplmain {
 
-	public static void main(String[] args) {
-
-		Controller c = new Controller();
-		c.run();
-
-	}
+    /**
+     * Método principal que se ejecuta al iniciar la aplicación.
+     * <p>
+     * Este método instancia la clase {@link Controller} y llama a su método
+     * <code>run</code> para iniciar la ejecución del programa.
+     * </p>
+     *
+     * @param args argumentos de línea de comandos (no utilizados)
+     */
+    public static void main(String[] args) {
+        Controller c = new Controller();
+        c.run();
+    }
 
 }
+
